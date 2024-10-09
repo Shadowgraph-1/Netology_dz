e16d268367d2260b55153024b9ca970a3448e81c
Shadowgraph <litvin4chuk@mail.ru>
2024-10-09 15:33:28 +0500

    Добавлен утерянный код!

---
+++ Бухгалтерия/dirty_main.py
@@ -0,0 +1,9 @@
+
+from application.salary import *
+from application.db.people import *
+from datetime import datetime
+
+if __name__ == '__main__':
+    print(f"Current date: {datetime.now().date()}")
+    calculate_salary()
+    get_employees()
